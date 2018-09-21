2225ed5032c04b6fb06e05843738de1c84494586
Thomas Johnson <tjohnson1106@gmail.com>
2018-09-21 13:17:32 -0400

    created register user mutation

--- lib/medium_api_web/schema.ex
+++ lib/medium_api_web/schema.ex
@@ -13,4 +13,12 @@ defmodule MediumApiWeb.Schema do
       resolve(&Resolvers.UserResolver.users/3)
     end
   end
+
+  mutation do
+    @desc "Register a new user"
+    field :register_user, type: :user_type do
+      arg(:input, non_null(:user_input_type))
+      resolve(&Resolvers.UserResolver.register_user/3)
+    end
+  end
 end
