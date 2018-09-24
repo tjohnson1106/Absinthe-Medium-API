c382606f6d9d18537a746101eb5bf14b1f1483df
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-09-24 14:24:18 -0400

    created context plug to accept authorization token

---
+++ lib/medium_api_web/plugs/context.ex
@@ -0,0 +1,21 @@
+defmodule MediumApiWeb.Plugs.Context do
+  @behaviour Plug
+
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def def call(conn, _) do
+    context = build_context(conn)
+    Absinthe.Plug.put_options(conn, context: context)
+  end
+
+  defp build_context(conn) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+         {:ok, user} <- MediumApi.Guardian.resource_from_claims(token) do
+      user
+    else
+      _ -> %{}
+    end
+  end
+end
