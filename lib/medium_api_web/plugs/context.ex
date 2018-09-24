22fc3536d09a84a10d79e7fc83ad01417d5245b2
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-09-24 15:44:12 -0400

    added context plug to router

--- lib/medium_api_web/plugs/context.ex
+++ lib/medium_api_web/plugs/context.ex
@@ -5,14 +5,15 @@ defmodule MediumApiWeb.Plugs.Context do
 
   def init(opts), do: opts
 
-  def def call(conn, _) do
+  def call(conn, _) do
     context = build_context(conn)
     Absinthe.Plug.put_options(conn, context: context)
   end
 
   defp build_context(conn) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
-         {:ok, user} <- MediumApi.Guardian.resource_from_claims(token) do
+         {:ok, claims} <- MediumApi.Guardian.decode_and_verify(token),
+         {:ok, user} <- MediumApi.Guardian.resource_from_claims(claims) do
       %{current_user: user}
     else
       _ -> %{}
